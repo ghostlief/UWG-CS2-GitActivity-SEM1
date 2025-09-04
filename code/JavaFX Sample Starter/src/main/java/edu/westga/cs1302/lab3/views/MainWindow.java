0fbd58baf363cbeb86a426e0fecb81e30fdfe616
ghostlief <aubreycollins62@gmail.com>
2025-09-04 12:26:23 -0400

    MainWindow Changes to FXML
    Created and added sample skeleton from scenebuilder
    Pasted skeleton into MainWindow.java

--- code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -6,28 +6,21 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-/**
- * Controller class for drawing various things to our canvas window.
- * 
- * @author CS 1302
- * @version Fall 2025
- */
 public class MainWindow {
+
     @FXML
-    private TextField input;
+    private TextField amount;
+
+    @FXML
+    private TextField name;
 
     @FXML
     private TextArea output;
 
     @FXML
-    void displayText(ActionEvent event) {
-    	String input = this.input.getText();
-    	this.output.setText(input);
+    void addItem(ActionEvent event) {
+
     }
-    /**
-     * Perform any needed initialization of UI components and underlying objects.
-     */
-    public void initialize() {
-    	
-    }
+
 }
+
