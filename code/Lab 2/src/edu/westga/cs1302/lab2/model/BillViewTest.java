9ec7392c54d4352431a9d47db8a2e3d8a46e04da
ghostlief <aubreycollins62@gmail.com>
2025-09-03 21:21:17 -0400

    Added BillViewTest class

---
+++ code/Lab 2/src/edu/westga/cs1302/lab2/model/BillViewTest.java
@@ -0,0 +1,15 @@
+package edu.westga.cs1302.lab2.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BillViewTest {
+
+	@Test
+	void getTextTest() {
+		
+		fail("Not yet implemented");
+	}
+
+}
