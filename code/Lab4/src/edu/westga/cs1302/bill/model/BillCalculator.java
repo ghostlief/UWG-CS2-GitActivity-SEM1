9e9c0fc508c6fddf39c008b7de26314baf363921
ghostlief <aubreycollins62@gmail.com>
2025-09-14 19:56:01 -0400

    calculateSubtotal parameter change
    changed the parameter to be an array of BillItem objects

--- code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -9,11 +9,13 @@ public class BillCalculator {
 	private static double subTotal = 0.0;
 	
 	/** its a method
-	 * @param bill a Bill object 
+	 * @precondition BillItem[] itemArray does not contain null values
+	 * 
+	 * @param itemArray an array of BillItem objects
 	 * @return subTotal a double representing the sub total of the sum of the bill items in an array
 	 */
-	public static double calculateSubtotal(Bill bill) {
-		for (BillItem item : bill.getItems()) {
+	public static double calculateSubtotal(BillItem[] itemArray) {
+		for (BillItem item : itemArray) {
 			subTotal += item.getAmount();
 		}
 		return subTotal;
