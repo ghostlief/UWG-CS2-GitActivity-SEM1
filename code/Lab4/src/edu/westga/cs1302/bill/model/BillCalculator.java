c2f76db969becbc732412a9202294bf1f56af71a
ghostlief <aubreycollins62@gmail.com>
2025-09-14 19:32:43 -0400

    calculate tax method
    added a method in the BillCalculator class called calculateTax
    returns the tax based on the subtotal

--- code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -6,17 +6,24 @@ package edu.westga.cs1302.bill.model;
  * @version Fall 2025
  */
 public class BillCalculator {
+	private static double subTotal = 0.0;
 	
 	/** its a method
 	 * @param bill a Bill object 
 	 * @return subTotal a double representing the sub total of the sum of the bill items in an array
 	 */
 	public static double calculateSubtotal(Bill bill) {
-		double subTotal = 0.0;
 		for (BillItem item : bill.getItems()) {
 			subTotal += item.getAmount();
 		}
 		return subTotal;
+	}
+	
+	/** calculates the tax
+	 * @return the tax 
+	 */
+	public static double calculateTax() {
+		return subTotal * Bill.TAX_RATE;
 	}
 
 }
