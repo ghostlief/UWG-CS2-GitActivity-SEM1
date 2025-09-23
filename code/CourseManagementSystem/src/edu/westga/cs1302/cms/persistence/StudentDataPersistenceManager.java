1adae736fd2d35590c09ac0686ac839f7dce4d5a
ghostlief <aubreycollins62@gmail.com>
2025-09-23 10:46:56 -0400

    Edited CourseManagementSystem, FileIO sample project
    edits to StudentDataPersistenceManager

--- code/CourseManagementSystem/src/edu/westga/cs1302/cms/persistence/StudentDataPersistenceManager.java
+++ code/CourseManagementSystem/src/edu/westga/cs1302/cms/persistence/StudentDataPersistenceManager.java
@@ -1,8 +1,16 @@
 package edu.westga.cs1302.cms.persistence;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 import edu.westga.cs1302.cms.model.Student;
 
-/** Suppports saving and loading student data,
+/** Supports saving and loading student data,
  * 
  * @author CS 1302
  * @version Fall 2025
@@ -15,9 +23,16 @@ public class StudentDataPersistenceManager {
 	 * @postcondition none
 	 * 
 	 * @param students the set of students to save
+	 * @throws IOException when unable to write to default file location
 	 */
-	public static void saveStudentData(Student[] students) {
-		
+	public static void saveStudentData(Student[] students) throws IOException {
+		try (FileWriter writer = new FileWriter("names.txt")) {
+			for (Student currStudent : students) {
+				writer.write(currStudent.getName() + System.lineSeparator());
+				writer.write(currStudent.getGrade());
+				writer.write(System.lineSeparator());
+			}
+		}
 	}
 
 	/** Load the students!
@@ -26,9 +41,23 @@ public class StudentDataPersistenceManager {
 	 * @postcondition none
 	 * 
 	 * @return the set of students loaded
+	 * @throws FileNotFoundException unable to find default file
+	 * @throws NumberFormatException file is not properly formatted, failed to convert a grade value to integer when reading
+	 * @throws NoSuchElementException file is not formatted properly, failed to read line when trying to read a grade value
 	 */
-	public static Student[] loadStudentData() {
-		return null;
+	public static Student[] loadStudentData() throws FileNotFoundException, NumberFormatException, NoSuchElementException {
+		ArrayList<Student> students = new ArrayList<Student>();
+		File inputFile = new File("names.txt");
+		try (Scanner reader = new Scanner(inputFile)) {
+			while (reader.hasNextLine()) {
+				String name = reader.nextLine();
+				int grade = Integer.parseInt(reader.nextLine());
+				Student nextStudent = new Student(name, grade);
+				students.add(nextStudent);
+			}
+		}
+		
+		return (Student[]) students.toArray();
 	}
 	
 }
