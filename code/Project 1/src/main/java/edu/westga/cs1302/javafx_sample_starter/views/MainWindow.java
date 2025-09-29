d49f5fdbddaec2fd6d9f008a77f8e97184418289
ghostlief <aubreycollins62@gmail.com>
2025-09-28 22:40:40 -0400

    FXML file and MainWindow changes
    added UI components textField, TextArea, Lables, buttons, and comboboxes
    added sample controller skeleton to MainWindow
    FXML file up to date

--- code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
+++ code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
@@ -1,5 +1,11 @@
 package edu.westga.cs1302.javafx_sample_starter.views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -9,10 +15,43 @@ package edu.westga.cs1302.javafx_sample_starter.views;
  */
 public class MainWindow {
     
+	@FXML
+    private TextArea taskDescription;
+
+    @FXML
+    private Button addTaskButton;
+
+    @FXML
+    private ComboBox<?> listView;
+
+    @FXML
+    private TextField taskName;
+
+    @FXML
+    private ComboBox<?> taskPriority;
+
+    @FXML
+    private Button viewTaskButton;
+	
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
     	
+    }
+
+    @FXML
+    void addTask(ActionEvent event) {
+
+    }
+
+    @FXML
+    void listTasks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewTask(ActionEvent event) {
+
     }
 }
